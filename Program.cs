b0af4b056054a0335fc5829ada0c2cb6098e0a15
zyong1 <zhiyuan.yong@illumina.com>
2024-02-27 11:11:15 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Cocona;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var builder = CoconaApp.CreateBuilder();
+builder.Services.AddLogging();
+builder.Configuration
+    .AddEnvironmentVariables()
+    .Build();
+
+var app = builder.Build();
+app.AddCommand((string inlineVariable,
+    [FromService] IConfiguration configuration,
+    ILogger<Program> logger) =>
+{
+    var configurationVariable = configuration["ConfigurationVariable"]!;
+    logger.LogInformation("Inline Variable: {InlineVariable}", inlineVariable);
+    logger.LogInformation("Configuration Variable: {ConfigurationVariable}", configurationVariable);
+});
+app.Run();
